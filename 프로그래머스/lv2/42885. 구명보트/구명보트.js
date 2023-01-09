664abf48daaa8d6082d027fbf744c77b7ac360b7
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-01-09 17:22:49 +0900

    [level 2] Title: 구명보트, Time: 29.99 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -2,13 +2,12 @@ function solution(people, limit) {
     var answer = people.length;
     const data = people.sort((a,b)=>
                            a-b )
-    for(i = 0, j = data.length-1 ; i < j ; i++){
+    for(i = 0, j = data.length-1 ; i < j;){
         if(data[i]+data[j] <= limit){
             answer --
             i++
         }
         j--
-        i--
     }
     return answer;
 }
