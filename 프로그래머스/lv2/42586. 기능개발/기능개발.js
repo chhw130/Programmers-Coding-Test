f54713a0da12938085ac2692576a4a869aa5980b
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-01-16 10:48:33 +0900

    [level 2] Title: 기능개발, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -0,0 +1,28 @@
+function solution(progresses, speeds) {
+    var answer = [];
+    const day = [];
+    let count = 0;
+    progresses = progresses.map((e)=>
+                  100-e)
+    for(i = 0 ; i < progresses.length ; i++){
+       day.push(Math.ceil(progresses[i]/speeds[i]))
+    }
+    let maxday = day[0]
+    for(i = 0, j = 0 ; i < day.length ; i++){
+        if(maxday >= day[i]){
+            count++
+            if(i === day.length-1){
+                answer.push(count)
+            }
+        }else{
+            maxday = day[i]
+            answer.push(count)
+            count = 1
+            if(i === day.length-1){
+                answer.push(count)
+            }
+        }
+        
+    }
+    return answer;
+}
