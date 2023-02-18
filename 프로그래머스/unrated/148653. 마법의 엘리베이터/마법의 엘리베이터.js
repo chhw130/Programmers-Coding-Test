ca47dc50c5a1df7eee4286d3d0eec4b9a680d396
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-02-18 23:39:25 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.js
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -1,12 +1,11 @@
 function remain(data){
     let count = 0
-    let data_set = data
     for(i = 1 ; i <= 10 ; i++){
         while(data % (10**i) !== 0){
         if(data % (10**i) > 5*(10**(i-1))){
             data = data+(10**(i-1))
             count++   
-        }else if(data % 10>=5 && parseInt(data/10) >=5){
+        }else if(data % 10 >=5 && parseInt(data/10) >=5){
             data = data+(10**(i-1))
             count++
         }else{
