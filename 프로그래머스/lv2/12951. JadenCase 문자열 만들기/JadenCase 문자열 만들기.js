0f398c8a4b3c8c5187ad55c94b0281ce2bfe4336
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-05-25 22:25:38 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,12 +1,17 @@
 function solution(s) {
     var answer = '';
-    const data = s.split(' ')
-     answer = data.map((props) =>{
-       if(props === ""){
-          return props 
-       }else {
-          return props[0].toUpperCase() + props.slice(1,props.length).toLowerCase();
+    let newAnswer = [];
+    answer =s.split(" ")
+    for(data of answer){
+       if(data.length > 0){
+         const newData =  data[0].toUpperCase() + data.substring(1).toLowerCase()
+        newAnswer.push(newData)
+       }else{
+           const newData = ""
+        newAnswer.push(newData)
+           
+           
        }
-     }) 
-    return answer.join(" ");
+    }
+    return newAnswer.join(" ")
 }
