b5ae0914772926b2f0834bda0ce9e9361a209eef
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-05-25 22:01:26 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,12 +1,10 @@
 function solution(s) {
     var answer = [];
-    let data = s.split(" ")
-    let data2 = data.sort((a,b)=>
-                 a-b )
-    let data3 = data2.map((e)=>
-                         parseInt(e))
-    let max = data3[0]
-    let min = data3[data3.length-1]
-    answer.push(max, min)
-    return answer.join(" ");
+   answer = s.split(" ")
+    answer.sort((a,b) => a-b)
+    
+    answer = answer[0] + " " + answer[answer.length-1]
+    
+  
+    return answer
 }
