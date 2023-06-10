ed1167ad73d2eb8882d36d31b29397c280b3e15c
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-06-10 21:18:39 +0900

    [unrated] Title: 카드 뭉치, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,27 @@
+function solution(cards1, cards2, goal) {
+    var answer = '';
+    let num = 0
+    
+    for(data of goal){
+        if(cards1[0] !== data && cards2[0] !== data){
+            return answer = "No"
+        }
+        if(cards1[0] === data){
+            cards1.shift()
+            num += 1
+        }
+        if(cards2[0] === data){
+            cards2.shift()
+            num += 1
+        }
+        
+        
+    }
+
+    if(num === goal.length){
+        answer = "Yes"
+    }
+
+    
+    return answer
+}
