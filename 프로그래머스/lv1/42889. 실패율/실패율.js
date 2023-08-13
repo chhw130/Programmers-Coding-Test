9a18c70cf57c723c0b2ceb05204155008ce31a6c
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-08-13 21:42:34 +0900

    [level 1] Title: 실패율, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -2,16 +2,12 @@ function solution(N, stages) {
     var answer = [];
     const stageObj = {}
     let people = stages.length
-    
-    
-  
-    
+
     stages.forEach(stage => {
 
         if(stage > N) return
         return stageObj[stage] = (stageObj[stage] || 0)  + 1
     })
-    
     
     for(let i = 1 ; i <= N ; i++){
         const stagePeople = stageObj[i]
