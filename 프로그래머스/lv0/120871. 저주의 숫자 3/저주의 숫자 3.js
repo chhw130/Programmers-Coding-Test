1938b61ca618435a0e845c5b6a1a8a72696d558b
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-08-20 21:44:35 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    var answer = 0;
+    
+    for(let i = 1 ; i<= n ; i++){
+        answer += 1
+        
+        while(answer % 3 === 0 || String(answer).includes("3")){
+            answer +=1
+            
+        }
+       
+    }
+    
+    return answer;
+}
