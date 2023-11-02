bf364ae24a181e59f17a49d8f3f13452d9d4920d
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-11-02 20:51:11 +0900

    [level 2] Title: n^2 배열 자르기, Time: 6.48 ms, Memory: 43.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -0,0 +1,12 @@
+function solution(n, left, right) {
+    var answer = [];
+    
+    for(i = left ; i <= right ; i++){
+        const row = Math.floor(i / n)
+        const col = i % n
+        const max = Math.max(row,col)
+        
+        answer.push(max+1)
+    }
+    return answer;
+}
