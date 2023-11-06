40c7d83776453bcd54154b02fac3f3765297bc80
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-11-06 13:40:25 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -0,0 +1,74 @@
+
+const calculateScore = (num, string, resultScore, i) => {
+    let score = parseInt(num)
+    const option = string.split("")    
+    
+    for(let j = 0; j < option.length ; j++){
+        const optionEle = option[j]
+        switch(optionEle){
+            case "S" :
+                score = score ** 1;
+                break;
+            case "D" :
+                score = score ** 2;
+                break
+            case "T" :
+                score = score ** 3;
+                break
+                
+            case "#" :
+                score = score * -1;
+                break;
+            case "*" :
+                score = score * 2
+                resultScore[i-1] *= 2
+                break;
+                
+                
+        }
+    }
+    resultScore.push(score)
+    return [score, resultScore]
+}
+
+const resultFunction = (arr) => {
+    let result = 0
+    const resultScore = []
+    for(let i = 0 ; i < arr.length ; i++){
+        const [num, string] = arr[i]
+        const [score , resultArr] = calculateScore(num, string, resultScore, i)
+        result += score
+    }
+    
+    return result = resultScore.reduce((a,b) => a+b, 0)
+    
+}
+
+function solution(dartResult) {
+    var answer = 0;
+    const arr = []
+
+    let numEle = ""
+    let stringEle = ""
+    
+    
+    for(let i = 0 ; i < dartResult.length ; i++){
+        const element = dartResult[i]
+        
+        
+        parseInt(element) || parseInt(element) === 0 ? numEle += element : stringEle += element
+        
+        if(!parseInt(element) && (parseInt(dartResult[i+1]) || dartResult[i+1] === "0")){
+            arr.push([numEle, stringEle])
+            numEle = ""
+            stringEle = ""
+        }else if(i === dartResult.length -1){
+            arr.push([numEle, stringEle])
+        }
+    }
+    
+    answer = resultFunction(arr)
+    
+    
+    return answer;
+}
