4737b305f8eeb728334c97627df004bcca2877cf
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-11-09 14:26:50 +0900

    [level 3] Title: 야근 지수, Time: 21.72 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.js
@@ -0,0 +1,29 @@
+function solution(n, works) {
+    var answer = 0;
+    const sortWorks = works.sort((a,b) => b-a)
+    const sum = sortWorks.reduce((a,b) => a+b, 0)
+    if(sum <= n) return answer = 0
+    
+    while(n > 0){
+        const maxEle = sortWorks[0] - 1
+        
+        
+        for(let i = 0 ; i < sortWorks.length ; i++ ){
+            const targetEle = sortWorks[i]
+            
+            if(targetEle >= maxEle){
+                n--
+                sortWorks[i]--
+                
+                if(n === 0) break
+            }
+            
+        }
+        
+    }
+    
+    
+    sortWorks.forEach((a) => answer += a**2)
+    
+    return answer;
+}
