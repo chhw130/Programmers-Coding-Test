39e0159fa2461c2f3b5d988f44e38f7355b90529
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-11-16 10:20:17 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -2,9 +2,7 @@ function solution(id_list, report, k) {
     var answer = [];
     const reportObj = {}
     const reportCountObj = {}
-    
     const newReport = report.map(e => e.split(" "))
-    
     
     for(let i = 0 ; i < newReport.length ; i++){
         const [reportPerson, targetPerson] = newReport[i]
@@ -12,11 +10,8 @@ function solution(id_list, report, k) {
         if(reportObj[reportPerson]?.includes(targetPerson)) continue
         
         reportCountObj[targetPerson] = reportCountObj[targetPerson] + 1 || 1
-        
         !reportObj[reportPerson] ? reportObj[reportPerson] = [targetPerson] : reportObj[reportPerson].push(targetPerson)
     }
-    
-    
     
     id_list.map((ele) => {
         const reportArr = reportObj[ele]
@@ -27,8 +22,5 @@ function solution(id_list, report, k) {
         }
         answer.push(temp)
     })
-    
-    
-    
     return answer;
 }
