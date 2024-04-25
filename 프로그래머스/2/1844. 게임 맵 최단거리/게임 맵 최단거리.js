4adfbc34c9ef1a6292fa47e64485a3290c7d2ba5
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2024-04-25 10:23:27 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 16.66 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,39 +1,27 @@
 function solution(maps) {
     var answer = -1;
-    const queue = [[0, 0, 1]]
     
+    const queue = [[0,0,1]]
     
     while(queue.length){
+        const [curX, curY, progress] = queue.shift()
         
-        const [xPos, yPos, progress] = queue.shift()
-        
-//          //**현재 위치 못가도록 false 처리 */
-//                 maps[xPos][yPos] = 0
-        
-        if(xPos === maps.length-1 && yPos === maps[0].length-1) {
+        if(curX === maps.length -1 && curY === maps[0].length -1){
             answer = progress
             break
         }
         
-       
-        
-        
-         //**상하좌우 이동할 수 있는 범위*/
-        const newPos = [ [xPos -1 , yPos], [xPos + 1 , yPos], [xPos, yPos -1], [xPos, yPos+1]  ]
-        
-        
-        /**상하좌우 중 갈수 있는 범위 push*/
-        for(let i = 0 ; i < 4 ; i ++){
-            const [newXPos, newYPos] = newPos[i]
+        const canGo = [[curX+1, curY], [curX-1, curY], [curX, curY+1], [curX, curY-1]];
+                       
+        for(let i = 0 ; i < 4 ; i++){
+            const [nextX, nextY] = canGo[i]
             
-           
-            if(maps[newXPos]?.[newYPos]){
-                maps[newXPos][newYPos] = 0
-                queue.push([newXPos, newYPos, progress+1])
+            if(maps[nextX]?.[nextY]){
+                maps[nextX][nextY] = 0
+                queue.push([nextX, nextY, progress+1])
             }
         }
-        
     }
     
-    return answer  ;
+    return answer;
 }
