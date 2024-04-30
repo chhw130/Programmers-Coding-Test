a7a2da479891f0433a49b6a3698236cc03a255de
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2024-04-30 21:25:18 +0900

    [level 2] Title: 피보나치 수, Time: 13.38 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    var answer = 0;
+    const arr = Array.from({length : 100000}, () => 0)
+    arr[1] = 1
+    
+    for(let i = 0 ; i < n ; i++){
+        arr[i+2] = (arr[i] + arr[i+1]) % 1234567
+    }
+    
+    console.log(arr[n] & 1234567)
+    
+    answer = arr[n] % 1234567
+    
+    return answer;
+}
