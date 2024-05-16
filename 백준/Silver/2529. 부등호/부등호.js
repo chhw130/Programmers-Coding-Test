acc1fe184c76b2099c77fe18a3cc1548abd9565d
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2024-05-16 14:46:46 +0900

    [Silver I] Title: 부등호, Time: 220 ms, Memory: 15320 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.js
+++ 백준/Silver/2529. 부등호/부등호.js
@@ -4,52 +4,56 @@ const filePath =
   process.platform === "linux" ? "/dev/stdin" : __dirname + "/test.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const [a, b] = input;
+const num = parseInt(input.shift());
 
-const arr = b.split(" ");
+const arr = input[0].split(" ");
 
-function solution(n, data) {
+function solution(n, boo) {
   let answer = [];
 
-  const memories = Array(10).fill(0);
+  const numArr = Array.from({ length: 10 }, () => 0);
 
-  const dfs = (count, target, copyMemo, number) => {
-    if (count === n) {
-      answer.push(number);
+  const dfs = (target, memoNum, turn, numText) => {
+    memoNum[target] = 1;
+
+    if (turn === n) {
+      if (numText.length === n + 1) {
+        answer.push(numText);
+      }
       return;
-    } else {
-      const check = data[count];
+    }
 
-      if (check === ">") {
-        for (let i = 0; i < 10; i++) {
-          if (!copyMemo[i] && i < target) {
-            const newMemo = copyMemo.slice();
-            newMemo[i] = 1;
-            dfs(count + 1, i, newMemo.slice(), `${number + i}`);
-          }
-        }
-      } else if (check === "<") {
-        for (let i = 0; i < 10; i++) {
-          if (!copyMemo[i] && i > target) {
-            const newMemo = copyMemo.slice();
-            newMemo[i] = 1;
-            dfs(count + 1, i, newMemo.slice(), `${number + i}`);
-          }
-        }
+    const check = boo[turn];
+
+    for (let j = 0; j < memoNum.length; j++) {
+      if (memoNum[j]) {
+        continue;
+      }
+      if (check === "<" && target < j) {
+        const newMemoNum = memoNum.slice();
+
+        dfs(j, newMemoNum, turn + 1, numText + j);
+      } else if (check === ">" && target > j) {
+        const newMemoNum = memoNum.slice();
+        dfs(j, newMemoNum, turn + 1, numText + j);
       }
     }
   };
 
-  for (let i = 0; i < 10; i++) {
-    const copyMemo = memories.slice();
-    copyMemo[i] = 1;
-    dfs(0, i, copyMemo, `${i}`);
+  for (let i = 0; i < numArr.length; i++) {
+    const copyNum = numArr.slice();
+    let temp = 0;
+    let text = "";
+    text += i;
+    dfs(i, copyNum, temp, text);
   }
 
-  answer.map((ele) => parseInt(ele)).sort((a, b) => a - b);
-  answer = [answer.shift(), answer.pop()];
+  answer.sort((a, b) => a - b);
 
-  return `${answer[1]}\n${answer[0]}`;
+  const max = answer[answer.length - 1];
+  const min = answer[0];
+
+  console.log(`${max}\n${min}`);
 }
 
-console.log(solution(parseInt(a), arr));
+solution(num, arr);
