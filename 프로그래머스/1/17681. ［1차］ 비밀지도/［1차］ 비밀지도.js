156ad679deb94fbc108b45fdb947386c525cf5ea
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2025-02-05 19:56:14 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,21 +1,39 @@
+
+const getPassword = (num1, num2) => {
+    
+    let password = ""
+    
+    for(let i = 0 ; i < num1.length ; i ++){
+        const temp1 = num1[i]
+        const temp2 = num2[i]
+        
+        if(temp1 === "1" || temp2 === "1"){
+            password += '#'
+            continue
+        }
+        
+        password += " "
+        
+    }
+    
+    return password
+    
+}
+
+
+
 function solution(n, arr1, arr2) {
     var answer = [];
-    for(i=0 ; i < arr1.length ; i++ ){
-       answer.push(arr1[i] | arr2[i])
+    
+    for(let i = 0 ; i < arr1.length ; i++){
+      const ele1 = arr1[i].toString(2).padStart(String(n), 0)
+      const ele2 = arr2[i].toString(2).padStart(String(n), 0)
+      
+      const password = getPassword(ele1, ele2)
+      
+        answer.push(password)
     }
-    answer = answer.map((e)=>{
-              e = [...e.toString(2)]
-        if((e.length < n)){
-            for(i = e.length ; e.length < n ; i++ ){
-                e.unshift(0)
-            }
-        }
-        return e.join("")
-    })
-    answer = answer.map((e)=>{
-                       e = e.replace(/0/g," ")
-                       e = e.replace(/1/g,"#")
-                    return e})
-        
+    
+    
     return answer;
 }
