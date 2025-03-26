7e1c1be7fc3705af1ab9ba6f1a6f7b389266ccf7
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2025-03-26 19:46:18 +0900

    [level 2] Title: 숫자의 표현, Time: 0.40 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,21 +1,13 @@
 function solution(n) {
-    var answer = 1;
-    
-    for(let i = 1 ; i < n/2 ; i++){
-        
-        let sum = 0
-        let j = i
-        
-        while(sum < n){
-            
-            sum += j
-            j++
-            if(sum === n){
-                answer++
-                break
-            }
+    var answer = 0;
+    const originN = n
+    while(n > 0){
+        if(n % 2 !== 0 && originN % n === 0 ){
+            answer += 1
         }
+            
+        n -= 1
+            
     }
-    
     return answer;
 }
