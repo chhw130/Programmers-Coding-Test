1ed1ebff0cabccacc50bfa9d2281793695c6bb92
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2025-04-03 12:20:03 +0900

    [level 2] Title: 귤 고르기, Time: 121.71 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/README.md
+++ 프로그래머스/2/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476#qna) 
 
 ### 성능 요약
 
-메모리: 48.3 MB, 시간: 60.26 ms
+메모리: 76.2 MB, 시간: 121.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 3일 15:7:18
+2025년 04월 03일 12:20:01
 
 ### 문제 설명
 
--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,26 +1,31 @@
-
-
-
-
 function solution(k, tangerine) {
     var answer = 0;
-    const tDict = {};
-    const arr = []
-    tangerine.forEach((t) => tDict[t] = (tDict[t] || 0) + 1);
     
-    for(i in tDict){
-        arr.push(tDict[i])
-    }
-    arr.sort((a,b) => b-a)
-   
-    for(i = 0 ; i < arr.length ; i++){
-       k = k-arr[i]
-        ++answer
-        if(k < 1 ){
+    const tangerineObj = {}
+    
+    tangerine.map(ele => {
+        tangerineObj[ele] = tangerineObj[ele] + 1 || 1 
+    })
+    
+    const seperateTangerine = Object.entries(tangerineObj).sort((a,b) => b[1] - a[1])
+    
+    
+    for(let i = 0 ; i < seperateTangerine.length ; i++){
+        
+        const [_ , number] = seperateTangerine[i]
+        
+        
+        
+        k -= number
+        answer++
+        
+        if(k <= 0){
             break
         }
-    
+      
+        
     }
+    
     
     return answer;
 }
